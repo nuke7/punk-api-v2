413ea78b03b4e8ade642f9b855dccbdf315bab7d
nuke7 <gombos.marton@icloud.com>
2021-03-31 21:57:59 +0200

    added react reveal

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,6 +1,7 @@
 import { useContext, useState } from "react";
 import { BeerContext } from "../Context";
 import { MyVerticallyCenteredModal } from "./Modal";
+import Zoom from "react-reveal/Zoom";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 
@@ -16,40 +17,42 @@ export const Grid = (props) => {
         {beer.length !== 0
           ? beer.map((beer, index) => {
               return (
-                <Card
-                  className="text-center"
-                  style={{ width: "18rem", minWidth: "240px", margin: "0.5rem" }}>
-                  <Card.Header>
-                    <h5>{beer.name}</h5>
-                  </Card.Header>
-                  <Card.Body>
-                    <Card.Title>{beer.tagline}</Card.Title>
-                    <Card.Text>
-                      <img
-                        src={
-                          beer.image_url
-                            ? `${beer.image_url}`
-                            : "https://s3.amazonaws.com/FringeBucket/image_placeholder.png"
-                        }
-                        alt="cover art"
-                        style={{ width: "auto", margin: "auto", maxHeight: "20vh" }}
-                      />
-                    </Card.Text>
-                    <Button
-                      id={index}
-                      onClick={(e) => {
-                        setId(e.currentTarget.id);
-                        console.log(id);
-                        setModalShow(true);
-                      }}
-                      variant="outline-info">
-                      Show More
-                    </Button>
-                  </Card.Body>
-                  <Card.Footer className="text-muted">
-                    First Brewed: {beer.first_brewed}
-                  </Card.Footer>
-                </Card>
+                <Zoom>
+                  <Card
+                    className="text-center"
+                    style={{ width: "18rem", minWidth: "240px", margin: "0.5rem" }}>
+                    <Card.Header>
+                      <h5>{beer.name}</h5>
+                    </Card.Header>
+                    <Card.Body>
+                      <Card.Title>{beer.tagline}</Card.Title>
+                      <Card.Text>
+                        <img
+                          src={
+                            beer.image_url
+                              ? `${beer.image_url}`
+                              : "https://s3.amazonaws.com/FringeBucket/image_placeholder.png"
+                          }
+                          alt="cover art"
+                          style={{ width: "auto", margin: "auto", maxHeight: "20vh" }}
+                        />
+                      </Card.Text>
+                      <Button
+                        id={index}
+                        onClick={(e) => {
+                          setId(e.currentTarget.id);
+                          console.log(id);
+                          setModalShow(true);
+                        }}
+                        variant="outline-info">
+                        Show More
+                      </Button>
+                    </Card.Body>
+                    <Card.Footer className="text-muted">
+                      First Brewed: {beer.first_brewed}
+                    </Card.Footer>
+                  </Card>
+                </Zoom>
               );
             })
           : "No such beer"}
